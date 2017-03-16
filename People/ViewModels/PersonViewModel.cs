f8c7d5fffc8476c231453df4c9b70565d6349d5d
Brett Morin <brettmorin2@gmail.com>
2017-03-16 12:51:27 -0700

    Made changes to use Delegate Commands

--- People/ViewModels/PersonViewModel.cs
+++ People/ViewModels/PersonViewModel.cs
@@ -1,15 +1,37 @@
-﻿using Business;
-using Main.Infrastructure; 
+﻿using System;
+using Business;
+using Main.Infrastructure;
+using Microsoft.Practices.Prism.Commands;
+using System.ComponentModel;
 
 namespace People
 {
   public class PersonViewModel : ViewModelBase, IPersonViewModel
   {
+    public DelegateCommand<Person> SaveCommand { get; set; }
+
     public PersonViewModel(IPersonView view)
         : base(view)
     {
       CreatePerson();
+      SaveCommand = new DelegateCommand<Person>(Save, CanSave);
     }
+
+    void OnAgeChanged(object sender, PropertyChangedEventArgs e)
+    {
+      if (e.PropertyName == "Error")
+        SaveCommand.RaiseCanExecuteChanged();
+    }
+
+    private void Save(Person value)
+    {
+      Person.LastUpdated = DateTime.Now.AddYears(value.Age);
+    }
+
+    private bool CanSave(Person value)
+    {
+      return Person.Error == null;
+    }           
 
     private Person _person;
     public Person Person
@@ -18,8 +40,14 @@ namespace People
       set
       {
         _person = value;
-        OnPropertyChanged("Person");
+        _person.PropertyChanged += Person_PropertyChanged;
+        OnPropertyChanged(nameof(Person));
       }
+    }
+
+    void Person_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+      SaveCommand.RaiseCanExecuteChanged();
     }
 
     private void CreatePerson()
