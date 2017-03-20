607f94a8f50c989e380b4b0772be3bdc34e3079f
Brett Morin <brettmorin2@gmail.com>
2017-03-20 13:49:43 -0700

    Added Application State Changed logic

--- ModuleA/ContentAViewModel.cs
+++ ModuleA/ContentAViewModel.cs
@@ -27,6 +27,19 @@ namespace ModuleA
         OnPropertyChanged("People");
       }
     }
+                             
+
+    private bool _IsBusy;
+
+    public bool IsBusy
+    {
+      get { return _IsBusy; }
+      set
+      {
+        _IsBusy = value;
+        OnPropertyChanged("IsBusy");
+      }
+    }                               
 
     #endregion //Properties
 
@@ -50,10 +63,12 @@ namespace ModuleA
 
     private void LoadPeople()
     {
+      IsBusy = true;
       _personService.GetPeopleAsync((sender, result) =>
       {
         People = new ObservableCollection<Person>(result.Object);
-      });
+        IsBusy = false;
+      });              
     }
 
     #endregion //Methods
