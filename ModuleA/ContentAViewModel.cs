7f88c961cd8cdbaa2fec9936571a3c2a5006c585
Brett Morin <brettmorin2@gmail.com>
2017-03-21 13:11:20 -0700

    Made changes to finish State Based Navigation

--- ModuleA/ContentAViewModel.cs
+++ ModuleA/ContentAViewModel.cs
@@ -8,6 +8,8 @@ using System.ComponentModel;
 using Main.Infrastructure.Services;
 using System.Collections.ObjectModel;
 using Business;
+using Microsoft.Practices.Prism.Commands;
+using Xceed.Wpf.Toolkit;
 
 namespace ModuleA
 {
@@ -16,6 +18,8 @@ namespace ModuleA
     private readonly IPersonService _personService;
 
     #region Properties
+    public DelegateCommand EditPersonCommand { get; private set; }
+
 
     private ObservableCollection<Person> _People;
     public ObservableCollection<Person> People
@@ -27,7 +31,18 @@ namespace ModuleA
         OnPropertyChanged("People");
       }
     }
-                             
+
+    private Person _SelectedPerson;
+    public Person SelectedPerson
+    {
+      get { return _SelectedPerson; }
+      set
+      {
+        _SelectedPerson = value;
+        EditPersonCommand.RaiseCanExecuteChanged();
+        OnPropertyChanged("SelectedPerson");
+      }
+    }
 
     private bool _IsBusy;
 
@@ -39,7 +54,19 @@ namespace ModuleA
         _IsBusy = value;
         OnPropertyChanged("IsBusy");
       }
-    }                               
+    }
+
+    private WindowState _WindowState;
+
+    public WindowState WindowState
+    {
+      get { return _WindowState; }
+      set
+      {
+        _WindowState = value;
+        OnPropertyChanged("WindowState");
+      }
+    }
 
     #endregion //Properties
 
@@ -49,14 +76,22 @@ namespace ModuleA
     {
       _personService = personService;
       LoadPeople();
+      EditPersonCommand = new DelegateCommand(EditPerson, CanEditPerson);
     }
-
+                        
     #endregion //Constructors
 
     #region Commands
+    private bool CanEditPerson()
+    {
+      return SelectedPerson != null;
+    }
 
-
-
+    private void EditPerson()
+    {
+      WindowState = WindowState.Open;
+    }
+              
     #endregion //Commands
 
     #region Methods
